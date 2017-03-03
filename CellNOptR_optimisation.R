7ce395684ca82580b3bf5f1ab1faecdd8dbcd400
eduati <eduati@ebi.ac.uk>
2017-03-03 16:57:52 +0100

    added package installation

--- CellNOptR_optimisation.R
+++ CellNOptR_optimisation.R
@@ -1,6 +1,17 @@
-# 
+### install required libraries
+# CellNOptR and MEIGOR can be found in Bioconductor and can be installed in R typing:
+source("https://bioconductor.org/biocLite.R")
+biocLite("CellNOptR")
+biocLite("MEIGOR")
+# CNORode2017 is available from github and can be installed in R typing:
+library(devtools)
+install_github("saezlab/CNORode2017")
+
+# NOTE: libraries should be loaded in this exact order:
+# (this is because CNORode is a dependency of MEIGOR and has same function names as CNORode2017
+# so it will be prioritizes when calling the optimisation function if loaded after CNORode2017)
 library(CellNOptR)
-library(CNORode)
+library(MEIGOR)
 library(CNORode2017)
 
 setwd("/Users/eduati/CPT_QSPtutorial/")
@@ -38,8 +49,14 @@ paramsSSm$maxNumSteps=10000;
 transferFun=4;
 paramsSSm$transfer_function = transferFun;
 
+paramsSSm$lambda_tau=0
+paramsSSm$lambda_k=0
+paramsSSm$bootstrap=F
+paramsSSm$SSpenalty_fac=10
+paramsSSm$SScontrolPenalty_fac=1000
+
 # run the optimisation algorithm
-opt_pars=parEstimationLBode(cnolist,model, method="essm", ode_parameters=ode_parameters, paramsSSm=paramsSSm, lambda=0)
+opt_pars=parEstimationLBode(cnolist,model, method="essm", ode_parameters=ode_parameters, paramsSSm=paramsSSm)
 
 # plot data with optimised parameters
 simulatedData=plotLBodeFitness(cnolist, model, transfer_function=transferFun, ode_parameters=opt_pars, reltol = 1e-6, atol = 1e-6, maxStepSize = 1)
